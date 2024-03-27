5fb39557cfb1ef312a0467a24b8ccd5f2e138bc2
yazgahar <battianas@hotmail.com>
2024-03-27 17:44:23 +0100

    function deletes added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,6 +45,12 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """function that deletes an object"""
+        if obj:
+            del (FileStorage.__objects["{}.{}".format
+                                       (obj.__class__.__name__, obj.id)])
