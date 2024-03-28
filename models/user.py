e2240249ce872fc77436db22c40201ea52c6da2d
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-28 09:26:05 +0100

    Add SQLAlchemy ORM mappings for User model

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by various attributes
+    and by the relationship between the user and the places"""
+    
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
+    
