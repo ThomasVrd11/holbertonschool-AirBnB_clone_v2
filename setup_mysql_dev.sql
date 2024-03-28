a0f78c1ca6c225b07495d9a893c51bcca31256cf
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-28 13:37:24 +0100

    Update console.py and setup_mysql_dev.sql, add test_console.py

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,7 @@
--- Description: This script is used to setup the development database for the AirBnB clone project.
--- It creates a database and a user with all privileges on the database.
-
+-- Create the hbnb_dev_db if it doesn't exist
+-- create user hbnb_dev with pwd hbnb_dev_pwd if it doesn't exist
+-- grants all privilege on hbnb_dev_db and select priv on performance_schema to user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- flush de merde
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,8 @@
--- Description: This script is used to setup the test database for the AirBnB clone project.
--- It creates a database and a user with all privileges on the database.
+-- Create the hbnb_test_db if it doesn't exist
+-- create user hbnb_test with pwd hbnb_test_pwd if it doesn't exist
+-- grants all privilege on hbnb_test_db and select priv on performance_schema to user
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- flush
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
