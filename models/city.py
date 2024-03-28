f5aab7015b1216720bed63c980c024970821a967
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-28 09:53:03 +0100

    Update City model with SQLAlchemy and relationships

--- models/city.py
+++ models/city.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+""" City Module for HBNB super project de merde"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name 
+    and a relationship with the state and places"""
+    
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    if storage_type == 'db':
+        state = relationship("State", back_populates="cities")
+        places = relationship("Place", backref="cities", cascade="delete")
+        
