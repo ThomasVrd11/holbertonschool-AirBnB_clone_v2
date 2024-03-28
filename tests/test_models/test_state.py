3bdf39d1ecbccb5fa1a24a5203fcf41138c8e4b8
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-28 15:21:51 +0100

    Fix unit test failures in test_models

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,22 +1,67 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
-from models.state import State
+import unittest
 import os
+from models.state import State
+from models.base_model import BaseModel
+import pep8
 
 
-class test_state(test_basemodel):
-    """test state class """
+class TestState(unittest.TestCase):
+    """ """
 
-    def __init__(self, *args, **kwargs):
-        """ test state class init"""
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
-
-    def test_name3(self):
+    @classmethod
+    def setUpClass(cls):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        cls.state = State()
+        cls.state.name = "CA"
+
+    @classmethod
+    def teardown(cls):
+        """ """
+        del cls.state
+
+    def tearDown(self):
+        """ """
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_Review(self):
+        """"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/state.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_State(self):
+        """ """
+        self.assertIsNotNone(State.__doc__)
+
+    def test_attributes_State(self):
+        """ """
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
+        self.assertTrue('name' in self.state.__dict__)
+
+    def test_is_subclass_State(self):
+        """ """
+        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
+
+    def test_attribute_types_State(self):
+        """ """
+        self.assertEqual(type(self.state.name), str)
+
+    def test_save_State(self):
+        """ """
+        self.state.save()
+        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+
+    def test_to_dict_State(self):
+        """ """
+        self.assertEqual('to_dict' in dir(self.state), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
