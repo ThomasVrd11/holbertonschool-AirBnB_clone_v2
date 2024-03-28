1484abccb2b11b284a1c2aa319ffba1be6ea1274
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-28 13:11:18 +0100

    Refactor models/__init__.py and models/base_model.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage or DBStorage"""
-""" depending on the value of the environment variable HBNB_TYPE_STORAGE"""
-
-
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
+
 if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
