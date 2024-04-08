262afea54915158cbe34f4f5fc94ac2f643ed575
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-08 11:18:55 +0200

    lecomit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+""" Script that starts a Flask web application zz"""
 
 from flask import Flask
 
@@ -13,3 +13,5 @@ def index():
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
+
+""" cc alfredo thx for watching"""
