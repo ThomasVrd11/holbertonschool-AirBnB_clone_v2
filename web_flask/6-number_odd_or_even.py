34729d4c8bdba917ad09f5efb718f0e83924b496
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-08 12:36:40 +0200

    wtf ?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -39,9 +39,7 @@ def number(n):
     return "{} is a number".format(n)
 
 
-app.route("/number_template/<int:n>", strict_slashes=False)
-
-
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """ html page with n as number """
     return render_template("5-number.html", n=n)
