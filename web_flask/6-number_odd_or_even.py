bbe838c5e94f24cd085312fa49d8b4b259b9d9db
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-08 12:29:08 +0200

    Add render_template import to Flask application

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,6 +47,12 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """n is odd ????????????????? or even !!!!!!!!!!!!"""
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
